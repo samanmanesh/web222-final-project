4ed7b174ecc8f2822b3d3764885163f18417884c
Saman Manesh <samanmanesh7@gmail.com>
2021-12-06 23:57:40 -0500

    did most of the contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,21 +6,60 @@ export default function Home() {
       <main className="grid gap-16 md:grid-cols-2">
         <div className="text-center md:text-left">
           <h1 className="mb-12">Contact me</h1>
-          <p className="text-2xl font-medium text-gray-700 mb-12 mx-auto md:ml-0 max-w-sm">
-            My name is Saman Manesh, <br />I'm a full-stack developer focused on
-            front-end based in Toronto.
-          </p>
-          <Link href="/about">
-            <span className="text-2xl font-medium select-none cursor-pointer">More about me &rarr;</span>
-          </Link>
+          <div>Email: Samansbm7@gmail.com</div>
+          <a href="">Github</a>
+          <a href="">LinkedIn</a>
         </div>
-        <div>
-          <div className="w-4/5 h-96 rounded-lg bg-gray-900 mx-auto"></div>
-        </div>
+        <form action="" className="grid gap-2" method="post">
+          <div className="flex ">
+            <div>
+              <label htmlFor="fname">First name</label>
+              <input
+                type="text"
+                name="fname"
+                className="bg-gray-900 rounded text-white h-8"
+              />
+            </div>
+            <div>
+              <label htmlFor="lname">Last name</label>
+              <input
+                type="text"
+                name="lname"
+                className="bg-gray-900 rounded h-8"
+              />
+            </div>
+          </div>
+          <label htmlFor="email">Email</label>
+          <input type="email" className="bg-gray-900 rounded  h-8" />
+          <label htmlFor="address">Address</label>
+          <input type="address" className="bg-gray-900 rounded  h-8" />
+          <label htmlFor="city">City</label>
+          <input list="country" className="bg-gray-900 rounded  text-white" />
+          <datalist id="country">
+            <option value="New York" />
+            <option value="Rome" />
+            <option value="London" />
+            <option value="Paris" />
+          </datalist>
+
+
+          <label htmlFor="state">Write me something</label>
+          <textarea name="state" className="bg-gray-900 rounded text-white" />
+            <div>
+          <input type="radio" id="QA" name="" value="Question"/>
+          <label htmlFor="QA">Question</label><br />
+          <input type="radio" id="CM" name="" value="Comment"/>
+          <label htmlFor="CM">Comment</label><br />
+          <input type="radio" id="HI" name="" value="Hiring"/>
+          <label htmlFor="HI">Hiring</label><br />
+          </div>
+
+          <input type="submit" value="Submit" className="bg-gray-900 rounded text-white" />
+          
+           {/* needs a hidden filed to have my name and student id */}
+
+        </form>
       </main>
     </div>
   );
 }
-   
-  
-
