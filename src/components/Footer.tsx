e221ad2b0021b9bc6a9cf7c621d170f3f337a0e6
Saman Manesh <samanmanesh7@gmail.com>
2021-12-07 17:06:11 -0500

    clean up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({}: Props): ReactElement {
     <footer className="wrapper flex items-center md:justify-between w-full">
       <p className="text-lg font-medium">&copy; Saman Manesh 2021</p>
       <div className="ml-auto flex items-center space-x-3">
-        <div className="w-8 h-8 rounded-full">
+        <div className="w-10 h-10 rounded-full hover:bg-red-400 hover:bg-opacity-5 grid place-items-center transition">
           <a
             href="https://www.linkedin.com/in/saman-manesh-96b257213/"
             target="_blank"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,22 @@ export default function Home() {
         <div className="text-center md:text-left">
           <h1 className="mb-12">Hi there!</h1>
           <p className="text-2xl font-medium text-gray-700 mb-12 mx-auto md:ml-0 max-w-sm">
-            My name is Saman Manesh, <br />I'm a full-stack developer focused on
-            front-end based in Toronto.
+            My name is Saman Manesh, <br />
+            I'm a full-stack developer focused on front-end based in Toronto.
           </p>
           <Link href="/about">
-            <span className="text-2xl font-medium select-none cursor-pointer">More about me &rarr;</span>
+            <span className="text-2xl font-medium select-none cursor-pointer">
+              More about me &rarr;
+            </span>
           </Link>
         </div>
         <div>
           <div className="w-4/5 h-196 rounded-lg bg-gray-900 mx-auto overflow-hidden">
-            <img  src="./Saman's Image2.jpeg" alt="Saman Manesh" className="w-full h-full object-cover" />
-
+            <img
+              src="./Saman's Image2.jpeg"
+              alt="Saman Manesh"
+              className="w-full h-full object-cover"
+            />
           </div>
         </div>
       </main>
